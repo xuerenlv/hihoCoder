90f169a7a77b7c2d28d8c9ac20cdc18427ad5de4
xuerenlv <xuerenlv@gmail.com>
2016-04-10 20:35:09 +0800

    r

---
+++ 2_Trie树.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+
+struct TrieNode{
+  bool is_word;
+  int count_1;
+  TrieNode *son;
+  TrieNode():is_word(false),count_1(0),son(NULL){};
+};
+
+void  insert_word(int start,const char* str,TrieNode* current) {
+  if(current->son == NULL){
+    current->son = new TrieNode[26];
+  }
+  current->count_1++;
+  if (str[start+1]=='\0') {
+    current->son[str[start]-'a'].is_word=true;
+    return;
+  }
+  insert_word(start+1, str, &(current->son[str[start]-'a']));
+}
+
+void query_word(int start,const char* str,TrieNode* current){
+  if(current->son==NULL)
+    std::cout << "0" << std::endl;
+  if(str[start+1]=='\0'){
+    std::cout << current->son[str[start]-'a'].count_1 << std::endl;
+  }else{
+    return query_word(start+1, str, &(current->son[str[start]-'a']));
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  int n;
+  char str[1000];
+  cin>>n;
+  TrieNode* root = new TrieNode();
+  for(int i=0;i<n;i++){
+    scanf("%s", str);
+    insert_word(0, str, root);
+  }
+
+  int m;
+  cin>>m;
+  for(int i=0;i<m;i++){
+    scanf("%s", str);
+    query_word(0, str, root);
+  }
+
+  return 0;
+}
