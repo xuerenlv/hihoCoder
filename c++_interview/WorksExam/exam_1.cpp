d11adb8560d7d7d8ea58e52ed0fe4346e0cfa606
xuerenlv <xuerenlv@gmail.com>
2016-06-02 10:25:10 +0800

    xhj

---
+++ c++_interview/WorksExam/exam_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <math.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  int n_test_case;
+
+  scanf("%d\n", &n_test_case);
+  for(int i=0;i<n_test_case;i++){
+
+
+  }
+  return 0;
+}
